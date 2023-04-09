072a43ff66eab3a347ccac712e68b28e03b6f857
Summer <nhamhuehue@gmail.com>
2023-04-09 15:14:56 +0700

    update source code

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const dropdowns = document.querySelectorAll('#menu ul li');
+// const dropdowns = document.querySelectorAll('#menu ul li');
 
-for (let i = 0; i < dropdowns.length; i++) {
-  dropdowns[i].addEventListener('click', function(event) {
-    event.stopPropagation();
-    if (this.querySelector('.sub-menu')) {
-      const subMenu = this.querySelector('.sub-menu');
-      if (subMenu.style.display === 'block') {
-        subMenu.style.display = 'none';
-      } else {
-        subMenu.style.display = 'block';
-      }
-    }
-  });
-}
+// for (let i = 0; i < dropdowns.length; i++) {
+//   dropdowns[i].addEventListener('click', function(event) {
+//     event.stopPropagation();
+//     if (this.querySelector('.sub-menu')) {
+//       const subMenu = this.querySelector('.sub-menu');
+//       if (subMenu.style.display === 'block') {
+//         subMenu.style.display = 'none';
+//       } else {
+//         subMenu.style.display = 'block';
+//       }
+//     }
+//   });
+// }
 
-document.addEventListener('click', function(event) {
-  const subMenus = document.querySelectorAll('.sub-menu');
-  for (let i = 0; i < subMenus.length; i++) {
-    subMenus[i].style.display = 'none';
-  }
-});
-//search
-function search() {
-    let searchText = document.getElementById("searchInput").value;
-    let searchBtn = document.getElementById("searchBtn");
+// document.addEventListener('click', function(event) {
+//   const subMenus = document.querySelectorAll('.sub-menu');
+//   for (let i = 0; i < subMenus.length; i++) {
+//     subMenus[i].style.display = 'none';
+//   }
+// });
+//Search
+
+// $(document).ready(function(){
+//     $(".searchBtn").on("click", function(){
+//       let searchInput = $(".searchInput").val().toLowerCase();
+//       $(".container *").filter(function() {
+//         $(this).toggle($(this).text().toLowerCase().indexOf(searchInput) > -1)
+//       });
+//     });
+//   });
   
-    // Bắt sự kiện click vào button "searchBtn"
-    searchBtn.addEventListener("click", function() {
-      let highlightedText = window.getSelection().toString();
-  
-      // Nếu có đoạn text được bôi đen thì tìm kiếm vào đoạn text đó
-      if (highlightedText !== "") {
-        searchText = highlightedText;
-      }
-  
-      // Tiến hành tìm kiếm
-      // ...
+$(document).ready(function(){
+    $(".searchBtn").on("click", function(){
+      let searchInput = $(".searchInput").val().toLowerCase();
+      $(".container *").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(searchInput) > -1)
+      }).addClass("highlight"); // Thêm class "highlight" vào các phần tử đã tìm thấy
     });
-  }
-  
+  });
   
